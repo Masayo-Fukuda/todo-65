fe4b0f5bb0747b2b7323837beafcaf65fe9f127f
MihiroHida <127464434+MihiroHida@users.noreply.github.com>
2023-03-30 11:18:58 +0800

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -10,7 +10,7 @@
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <form action="{{ route('posts.store') }}" method="POST">
+                <form action="" method="POST">
                 @csrf
                 <div class="form-group">
                     <label>タイトル</label>
