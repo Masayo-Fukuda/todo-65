299ff717b96d0e49be19d7c101cb236865b85d8b
HYAHAAG <bananaironohiyoko@gmail.com>
2023-03-30 12:16:52 +0900

    index基本的な記述
    タイトル
    説明
    画像
    編集ボタン
    削除ボタンの骨組み

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>投稿一覧</div>
+
+    <div>
+        <h1>タスクの題名</h1>
+        <p>タスクの説明文</p>
+        <img src="画像のファイルパス" alt="画像の説明">
+        {{-- 編集ボタン --}}
+        <button type="button">ボタン1</button>
+        {{-- 削除のボタン --}}
+        <button type="button">ボタン2</button>
+        <a href="#" >
+            新規投稿
+        </a>
+    </div>
+
+</body>
+
+</html>
