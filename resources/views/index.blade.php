fc559cdf5137050f9cec374146da311028c40939
HYAHAAG <bananaironohiyoko@gmail.com>
2023-03-30 22:25:27 +0900

    index.blade.phpルートにつなげる
    タイトル、内容、画像、編集、削除などをルートにつなげる

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,18 +10,24 @@
 <body>
     <div>投稿一覧</div>
 
+    @foreach ($tasks as $task )
     <div>
-        <h1>タスクの題名</h1>
-        <p>タスクの説明文</p>
-        <img src="画像のファイルパス" alt="画像の説明">
-        {{-- 編集ボタン --}}
-        <button type="button">ボタン1</button>
-        {{-- 削除のボタン --}}
-        <button type="button">ボタン2</button>
-        <a href="#" >
-            新規投稿
-        </a>
+        <h1>タスクの題名: {{ $task->title }}</h1>
+        <p>タスクの説明文:{{ $task->body }}</p>
+        {{-- 画像 --}}
+        <img src="{{ asset('storage/image/'.$task->image_at) }}" alt="{{ $task->title }}" width="200">
+        {{-- <img src="{{ route('task->img_at') }}" alt="画像の説明"> --}}
+        {{-- <img src="{{ asset('images/' . $task->image) }}" alt="{{ $task->title }}" width="200px"> --}}
+        {{-- 編集 --}}
+        <a href="{{ route('tasks.edit',$task->id) }}" >編集する</a>
+        {{-- 削除 --}}
+        <form action='{{ route('tasks.destroy',$task->id) }}' method='post'>
+            @csrf
+            @method('delete')
+            <input type='submit' value='削除'  onclick='return confirm("本当に削除しますか？");'>
+        </form>
     </div>
+    @endforeach
 
 </body>
 
