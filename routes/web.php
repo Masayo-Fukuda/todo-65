5b8a8d0f9ba6bb910e052fb250350d156d73538e
MihiroHida <127464434+MihiroHida@users.noreply.github.com>
2023-03-30 18:31:31 +0800

    ルート追加
    k

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/tasks',[App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/tasks/create',[App\Http\Controllers\TaskController::class, 'create'])->name('tasks.create');
+
+Route::post('/tasks',[App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
+
+Route::get('/tasks/{id}/edit',[App\Http\Controllers\TaskController::class, 'edit'])->name('tasks.edit');
+
+Route::put('/tasks/{id}',[App\Http\Controllers\TaskController::class, 'update'])->name('tasks.update');
+
+Route::delete('/tasks/{id}',[App\Http\Controllers\TaskController::class, 'destroy'])->name('tasks.destroy');
