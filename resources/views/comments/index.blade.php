1defdeb8fa448835a4b67dff3b7b51afc6d8868b
HYAHAAG <bananaironohiyoko@gmail.com>
2023-04-04 12:37:35 +0900

    comment修正

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -10,9 +10,7 @@
 <body>
   <header>
     <div class="left">
-        <a href="{{ url('/') }}">
-            ToDo
-        </a>
+        <a href="{{ url('/') }}">ToDo</a>
     </div>
     <div class="right">
         <a href="{{ route('tasks.index') }}">投稿一覧へ</a>
@@ -20,25 +18,31 @@
             @if (Route::has('login'))
                 <a href="{{ route('login') }}">{{ __('Login') }}</a>
             @endif
-
             @if (Route::has('register'))
                 <a href="{{ route('register') }}">{{ __('Register') }}</a>
             @endif
         @else
-            <a id="navbarDropdown"  href="{{ route('mypage.show', Auth::user()->id ) }}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}さんのマイページ
-            </a>
+            <a id="navbarDropdown"  href="{{ route('mypage.show', Auth::user()->id ) }}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }}さんのマイページ</a>
         @endguest
     </div>
 </header>
-  <h1>コメント一覧</h1>
+
+<main>
+  <div class=main>
+    <p class=title>コメント一覧</p>
+</div>
+  {{-- <h1>タスクの題名: {{ $task->title }}</h1>
+  <p>タスクの説明文:{{ $task->contents }}</p> --}}
+  <div class="main">
+    @foreach($comments as $comment)
+      <div>
+        <p>投稿者:{{ $comment->user->name }}</p>
+        <br>
+        <p>コメント内容<br>{{ $comment->body }}</p>
+      </div>
+    @endforeach
+</div>
   <a href="{{ route('tasks.index') }}">投稿一覧へ戻る</a>
-  
-  @foreach($comments as $comment)
-    <div>
-      <p>投稿者:{{ $comment->user->name }}</p>
-      <p>内容：{{ $comment->body }}</p>
-    </div>
-  @endforeach
+</main>
 </body>
 </html>
