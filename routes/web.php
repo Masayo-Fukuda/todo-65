c31f927ae55f74903bd079d095118a02b7ba77e5
Masayo-Fukuda <126930383+Masayo-Fukuda@users.noreply.github.com>
2023-04-04 22:18:26 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\BookmarksController;
+use App\Http\Controllers\MypageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,32 +24,32 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
 // タスクの検索
-Route::post('/tasks',[App\Http\Controllers\TaskController::class, 'search'])->name('tasks.search');
+Route::post('/tasks',[TaskController::class, 'search'])->name('tasks.search');
 
 // タスクの CRUD
-Route::get('/tasks',[App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/create',[App\Http\Controllers\TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks',[App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
-Route::get('/tasks/{id}/edit',[App\Http\Controllers\TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('/tasks/{id}',[App\Http\Controllers\TaskController::class, 'update'])->name('tasks.update');
-Route::delete('/tasks/{id}',[App\Http\Controllers\TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::get('/tasks',[TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create',[TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks',[TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks/{id}/edit',[TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{id}',[TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{id}',[TaskController::class, 'destroy'])->name('tasks.destroy');
 
 //コメント機能
-Route::get('/comments/create/{task_id}',[App\Http\Controllers\CommentController::class, 'create'])->name('comments.create');
-Route::post('/comments/store',[App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
-Route::get('/comments/{id}', [App\Http\Controllers\CommentController::class, 'index'])->name('comments.index');
+Route::get('/comments/create/{task_id}',[CommentController::class, 'create'])->name('comments.create');
+Route::post('/comments/store',[CommentController::class, 'store'])->name('comments.store');
+Route::get('/comments/{id}', [CommentController::class, 'index'])->name('comments.index');
 
 // bookmark
-Route::post('/bookmarks', [App\Http\Controllers\BookmarksController::class, 'store'])->name('bookmarks.store');
+Route::post('/bookmarks', [BookmarksController::class, 'store'])->name('bookmarks.store');
 
-Route::delete('/bookmarks/{bookmark}', [App\Http\Controllers\BookmarksController::class, 'destroy'])->name('bookmarks.destroy');
+Route::delete('/bookmarks/{bookmark}', [BookmarksController::class, 'destroy'])->name('bookmarks.destroy');
 
 // マイページ
-Route::get('/mypage/{id}', [App\Http\Controllers\MypageController::class, 'show'])->name('mypage.show');
+Route::get('/mypage/{id}', [MypageController::class, 'show'])->name('mypage.show');
 
 
 
